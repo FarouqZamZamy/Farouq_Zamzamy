de8f39f9a73e9ecad21fb3213e0e242eb591dff5
FarouqZamZamy <farouqzamzamy24@gmail.com>
2025-11-07 21:13:30 +0700

    commit pertama

--- Java.js
+++ Java.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 document.getElementById("darkModeBtn").addEventListener("click", function () {
 document.body.classList.toggle("dark-mode");
 });
+=======
+document.getElementById("darkModeBtn").addEventListener("click", function () {
+document.body.classList.toggle("dark-mode");
+});
+>>>>>>> 129e062 (commit pertama)

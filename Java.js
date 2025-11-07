23d53d0f637600299eb59670d45190190f8ad4a2
FarouqZamZamy <farouqzamzamy24@gmail.com>
2025-11-07 22:28:25 +0700

    commit kedelapan

--- Java.js
+++ Java.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("darkModeBtn").addEventListener("click", function () {
 document.body.classList.toggle("dark-mode");
 });
-=======
-document.getElementById("darkModeBtn").addEventListener("click", function () {
-document.body.classList.toggle("dark-mode");
-});
->>>>>>> 129e062 (commit pertama)
